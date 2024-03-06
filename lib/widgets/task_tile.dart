bac009d61210b1d8288281eba1c74f7013710d98
shin.tu <shin.tu@allionsoft.com>
2024-03-06 14:36:08 +0800

    first commit

---
+++ lib/widgets/task_tile.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+class TaskTile extends StatelessWidget {
+  final String title;
+  final bool isDone;
+  final Function(bool) onChanged;
+
+  TaskTile({
+    required this.title,
+    required this.isDone,
+    required this.onChanged,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      title: Text(title),
+      leading: Checkbox(
+        value: isDone,
+        onChanged: (value) {
+          // Implement task completion logic
+          print('value = $value');
+        },
+      ),
+    );
+  }
+}
